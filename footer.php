fd8669f9887a8290a689ba31255a3f65a9949b1b
Michael <49420226+mikeed1998@users.noreply.github.com>
2024-01-11 16:34:12 -0600

    responsive del footer mejorado, solo checar detalles

--- footer.php
+++ footer.php
@@ -2,9 +2,263 @@
 
     $footer = '
         <footer>
-            <div class="container-fluid bg-dark text-white p-5 fs-1">
-                Footer
+            <div class="container-fluid bg-black p-5">
+                <div class="row">
+                    <div class="col border border-white">
+                        <div class="row py-2">
+                            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-12 border-end border-white">
+                                <form action="datos.php" class="needs-validation" method="POST" enctype="multipart/from-data">
+                                    <div class="row">
+                                        <div class="col-9 mx-auto py-5">
+                                            <div class="row">
+                                                <div class="col-xxl-9 col-xl-9 col-lg-11 col-md-11 col-sm-11 col-11 text-start text-white fs-3">
+                                                    Necesitas ayuda? Escribenos
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-xxl-9 col-xl-9 col-lg-11 col-md-11 col-sm-11 col-11 text-white">
+                                                    Deja tu correo y tu comentario y un asesor se reportará contigo a la brevedad
+                                                </div>
+                                            </div>
+                                            <div class="row mt-3">
+                                                <div class="col-12 text-white">
+                                                    <div class="row py-2">
+                                                        <div class="col-xxl-6 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mx-auto text-start">
+                                                            <input type="text" id="nombre" class="form-control fs-5 border border-white bg-black footer-input" placeholder="NOMBRE" required>
+                                                            <div class="valid-feedback">
+                                                                Correcto
+                                                            </div>
+                                                            <div class="invalid-feedback">
+                                                                Ingresa un nombre
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xxl-6 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mx-auto mt-xxl-0 mt-xl-3 mt-lg-3 mt-md-3 mt-sm-3 mt-3 py-xxl-0 py-xl-2 py-lg-2 py-md-2 py-sm-2 py-2">
+                                                            <input type="number" id="whatsapp" class="form-control fs-5 border border-white bg-black footer-input" placeholder="WHATSAPP" required>
+                                                            <div class="valid-feedback">
+                                                                Correcto
+                                                            </div>
+                                                            <div class="invalid-feedback">
+                                                                No valido
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row mt-1">
+                                                <div class="col-12 text-white">
+                                                    <div class="row py-2">
+                                                        <div class="col text-start">
+                                                            <input type="text" id="mensaje" class="form-control fs-5 border border-white bg-black footer-input" placeholder="MENSAJE" required>
+                                                            <div class="valid-feedback">
+                                                                Correcto
+                                                            </div>
+                                                            <div class="invalid-feedback">
+                                                                No valido
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row mt-1">
+                                                <div class="col-12 text-white">
+                                                    <div class="row py-2">
+                                                        <div class="col text-start">
+                                                            <input type="submit" id="footer-enviar" class="form-control" value="ENVIAR">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-12">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="row">
+                                            <div class="col p-5 text-end text-white">
+                                                <div class="row">
+                                                    <div class="col-xxl-6 col-xl-6 col-lg-1 col-md-1 col-sm-12 col-12"></div>
+                                                    <div class="col-6 col-xl-6 col-lg-11 col-md-11 col-sm-12 col-12 mx-auto aling-content-center justify-content-center">      
+                                                        <div class="row">
+                                                            <div class="col-xxl-9 col-xl-9 col-lg-9 col-md-10 col-sm-9 col-12 fs-5 mt-1">
+                                                                ¡Hola Pau! / <a href="#/" class="link-contacto">Cerrar sesión</a>
+                                                            </div>
+                                                            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-5 mt-xxl-0 mt-xl-0 mt-lg-0 mt-md-0 mt-sm-0 mt-4 text-start mx-xxl-0 mx-xl-0 mx-lg-0 mx-md-0 mx-sm-auto mx-auto">
+                                                                <a href="">
+                                                                    <div class="row">
+                                                                        <div class="col position-relative">
+                                                                            <img src="img/carrito/logo2.png" alt="" class="img-fluid">
+                                                                            <div class="col position-absolute top-0 start-50 translate-middle-y">
+                                                                                <span class="badge rounded-circle medalla-color-foot p-2">+9</span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-11 mx-auto">
+                                                <div class="row">
+                                                    <div class="col-xxl-6 col-xl-6 col-lg-6 text-white display-5">
+                                                        <div class="row">
+                                                            <div class="col py-1">
+                                                                <a href="#/" class="link-contacto">Home</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col py-1">
+                                                                <a href="#/" class="link-contacto">Rebajas</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col py-1">
+                                                                <a href="#/" class="link-contacto">Contacto</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xxl-6 col-xl-6 col-lg-6">
+                                                        <div class="row mt-5">
+                                                            <div class="col-4"></div>
+                                                            <div class="col-8 text-secondary text-end mt-5 py-2">
+                                                                Avenida Lapizlazuli 2074 int3 Residencial Victoria, Guadalajara, Jalisco, México contacto@rfc.com 3338096501.
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-12 py-3">
+                                                <div class="col-12 border-top border-white"></div>
+                                            </div>
+                                        </div>
+                                        <div class="row mt-1 mb-2 text-white">
+                                            <div class="col-11 mx-auto">
+                                                <div class="row">
+                                                    <div class="col-xxl-4 col-xl-4 col-lg-3 col-md-6 col-sm-6 col-6">
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="#/" class="link-contacto">Aviso de privacidad</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="#/" class="link-contacto">Preguntas frecuentes</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="#/" class="link-contacto">Terminos y condiciones</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xxl-4 col-xl-4 col-lg-3 col-md-6 col-sm-6 col-6">
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="#/" class="link-contacto">Envio y entrega</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="#/" class="link-contacto">Devoluciones</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="#/" class="link-contacto">Pagos</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-12 col-sm-12 col-12">
+                                                        <div class="row">
+                                                            <div class="col fs-1 text-end">
+                                                                <a href="#/" class="link-contacto_social">
+                                                                    <i class="bi bi-whatsapp"></i>
+                                                                </a>
+                                                                <a href="#/" class="link-contacto_social">
+                                                                    <i class="bi bi-facebook"></i>
+                                                                </a>
+                                                                <a href="#/" class="link-contacto_social">
+                                                                    <i class="bi bi-instagram"></i>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col py-2 fs-5 text-center text-secondary">
+                        Valeria Bazante 2023 todos los derechos reservados diseño por Wozial
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col text-center">
+                        <img src="img/footer/uve.png" alt="" class="img-fluid">
+                    </div>
+                </div>
             </div>
         </footer>
+        
+        <script>
+            document.getElementById(\'nombre\').addEventListener(\'input\', function () {
+                var nombreInput = this.value.trim();
+                var feedback = this.nextElementSibling;
+            
+                if (nombreInput.length > 0) {
+                    this.classList.remove(\'is-invalid\');
+                    this.classList.add(\'is-valid\');
+                    feedback.textContent = \'Correcto\';
+                } else {
+                    this.classList.remove(\'is-valid\');
+                    this.classList.add(\'is-invalid\');
+                    feedback.textContent = \'Ingresa un nombre válido\';
+                }
+            });
+            
+            document.getElementById(\'whatsapp\').addEventListener(\'input\', function () {
+                var whatsappInput = this.value.trim();
+                var feedback = this.nextElementSibling;
+            
+                var minDigits = 10; 
+            
+                if (whatsappInput.length >= minDigits && /^\d+$/.test(whatsappInput)) {
+                    this.classList.remove(\'is-invalid\');
+                    this.classList.add(\'is-valid\');
+                    feedback.textContent = \'Correcto\';
+                } else {
+                    this.classList.remove(\'is-valid\');
+                    this.classList.add(\'is-invalid\');
+                    feedback.textContent = \'Ingresa un número de teléfono válido con al menos \' + minDigits + \' dígitos\';
+                }
+            });
+            
+            document.getElementById(\'mensaje\').addEventListener(\'input\', function () {
+                var mensajeInput = this.value.trim();
+                var feedback = this.nextElementSibling;
+            
+                if (mensajeInput.length > 0) {
+                    this.classList.remove(\'is-invalid\');
+                    this.classList.add(\'is-valid\');
+                    feedback.textContent = \'Correcto\';
+                } else {
+                    this.classList.remove(\'is-valid\');
+                    this.classList.add(\'is-invalid\');
+                    feedback.textContent = \'Ingresa un mensaje válido\';
+                }
+            });
+        </script>
+
     ';
 
